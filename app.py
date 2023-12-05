70fc24133ae899e45f06001b6b930874b6aa52d9
VILLACIS_MICHAEL <135200029+MRVB22@users.noreply.github.com>
2023-12-04 20:02:54 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template('pokemon.html')
+
+@app.route("/detalle")
+def detalle():
+    return render_template('detalle.html')
+
+if __name__=='__main__':
+    app.run(debug=True)

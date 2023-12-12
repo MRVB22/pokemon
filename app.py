d3494c6c818d00e392bfd48e5681f8252def86b1
VILLACIS_MICHAEL <135200029+MRVB22@users.noreply.github.com>
2023-12-11 19:53:16 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,jsonify
 import requests 
+
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import Mapped, mapped_column
+
+
 app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///pokedex.sqlite"
+
+db = SQLAlchemy(app)
+
+
+class Pokemon(db.Model):
+    id: Mapped[int] = mapped_column(db.Integer, primary_key=True,autoincrement=True)
+    name: Mapped[str] = mapped_column(db.String, unique=True, nullable=False)
+
+
+with app.app_context():
+    db.create_all()
 
 
 def get_pokemon_data(pokemon):
-
     url = f'https://pokeapi.co/api/v2/pokemon/{pokemon}'
     r = requests.get(url).json()
     return r
@@ -16,6 +34,33 @@ def home():
 @app.route("/detalle")
 def detalle():
     return render_template('detalle.html')
+
+
+@app.route("/insert")
+def insert():
+    new_pokemon= 'Ditto'
+    if new_pokemon:
+            obj = Pokemon(name=new_pokemon)
+            db.session.add(obj)
+            db.session.commit()
+    return 'Pokemon Agregado'
+
+@app.route("/select")
+def select():
+    lista_pokemon= Pokemon.query.all()
+    for p in lista_pokemon:
+        print(p.name)
+
+    return 'alo'
+
+
+@app.route("/select/<name>")
+def selectbyname(name):
+    poke= Pokemon.query.filter_by(name=name).first()
+    return str(poke.id)
+
+
+
 
 if __name__=='__main__':
     app.run(debug=True)

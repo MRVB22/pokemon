040ecd00a8cd21f35c4717bde5a8d5b1836fcdf7
VILLACIS_MICHAEL <135200029+MRVB22@users.noreply.github.com>
2023-12-18 19:50:44 -0500

    Update app.py

--- app.py
+++ app.py
@@ -38,20 +38,24 @@ def search_pokemon(name):
     data = get_pokemon_data(name.lower())
     if 'sprites' not in data or 'official-artwork' not in data['sprites']['other']:
         return render_template('pokemon.html', pokemon=None)
-    
     pokemon = {
         'photo': data['sprites']['other']['official-artwork']['front_default'],
         'name': data['name'].upper(),
         'height': data['height'],
         'weight': data['weight'],
         'order': data['order'], 
-        'type': '',
+        'type': ' Estudiante',
+        'hp': data.get('stats')[0].get('base_stat'),
+        'attack': data.get('stats')[1].get('base_stat'),
+        'defence': data.get('stats')[2].get('base_stat'),
+        'speed': data.get('stats')[5].get('base_stat'),
     }
+
     return render_template('pokemon.html', pokemon=pokemon)
 
-@app.route("/detalle")
-def detalle():
-    return render_template('detalle.html')
+@app.route("/detalle/<hp>/<attack>/<defence>/<speed>/")
+def detalle(hp,attack,defence,speed):
+    return render_template('detalle.html',hp = hp, attack = attack, defence = defence, speed = speed)
 
 @app.route("/insert_pokemon/<pokemon>")
 def insert(pokemon):

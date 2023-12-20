6d46c23d2dd881f01a1437deacddf7adbe16ea8b
VILLACIS_MICHAEL <135200029+MRVB22@users.noreply.github.com>
2023-12-19 23:14:16 -0600

    Update app.py

--- app.py
+++ app.py
@@ -38,22 +38,28 @@ def search_pokemon(name):
     data = get_pokemon_data(name.lower())
     if 'sprites' not in data or 'official-artwork' not in data['sprites']['other']:
         return render_template('pokemon.html', pokemon=None)
+    
+    official_artwork = data['sprites']['other'].get('official-artwork', {}).get('front_default')
+    dream_artwork = data['sprites']['other'].get('dream-artwork', {}).get('front_default')
+
+    if not official_artwork:
+        return render_template('pokemon.html', pokemon=None)
+
     pokemon = {
         'name': data['name'].upper(),
         'height': data['height'],
         'weight': data['weight'],
-        'order': data['order'], 
-        'type': ' Estudiante',
+        'order': data['order'],
+        'type': 'Estudiante',
         'hp': data.get('stats')[0].get('base_stat'),
         'attack': data.get('stats')[1].get('base_stat'),
         'defence': data.get('stats')[2].get('base_stat'),
         'speed': data.get('stats')[5].get('base_stat'),
-        'photo': data['sprites']['other']['official-artwork']['front_default'],
-        'photo1': data['sprites']['other']['dream-artwork']['front_default'],
+        'photo': official_artwork,
+        'photo1': dream_artwork if dream_artwork else ''  
     }
 
     return render_template('pokemon.html', pokemon=pokemon)
-
 @app.route("/detalle/<hp>/<attack>/<defence>/<speed>/")
 def detalle(hp,attack,defence,speed):
     return render_template('detalle.html',hp = hp, attack = attack, defence = defence, speed = speed)
